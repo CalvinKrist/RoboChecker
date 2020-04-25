6cc466b4bb7c56940767d6add5ecda512eeacef0
CalvinKrist <calvin.krist@yahoo.com>
2020-04-25 15:35:54 -0400

    Upgrade path genertor for increased automation

--- path_generator.bat
+++ path_generator.bat
@@ -1,12 +1,7 @@
 @echo off
 
-:: Generate model
-python spiral_model_generator.py %1 > model.pm
-
-:: Run prism
-
 :: Set prism directory (no quotes)
-set PRISM_DIR=C:\Program Files\prism-4.5
+set PRISM_DIR=D:\Program Files\prism-4.5
 
 :: Add PRISM to path
 path=%PRISM_DIR%\lib;%path%
@@ -19,5 +14,14 @@ path=%PRISM_DIR%\lib;%path%
 ::  - lib/*.jar (all other jars)
 set CP=%PRISM_DIR%\lib\prism.jar;%PRISM_DIR%\classes;%PRISM_DIR%;%PRISM_DIR%\lib\pepa.zip;%PRISM_DIR%\lib\*
 
-:: Run PRISM through Java with sufficient memory
-java -Xmx4g -Xss8M -Djava.library.path="%PRISM_DIR%\lib" -classpath "%CP%" prism.PrismCL model.pm -simpath %2 %3
+:: Generate spiral path
+python spiral_model_generator.py %1 > model.pm
+java -Xmx4g -Xss8M -Djava.library.path="%PRISM_DIR%\lib" -classpath "%CP%" prism.PrismCL model.pm -simpath %2 spiral/m%1paths/%3
+
+:: Generate random path
+python random_model_generator.py %1 > model.pm
+java -Xmx4g -Xss8M -Djava.library.path="%PRISM_DIR%\lib" -classpath "%CP%" prism.PrismCL model.pm -simpath %2 random/m%1paths/%3
+
+:: Generate snake path
+python snake_model_generator.py %1 > model.pm
+java -Xmx4g -Xss8M -Djava.library.path="%PRISM_DIR%\lib" -classpath "%CP%" prism.PrismCL model.pm -simpath %2 snake/m%1paths/%3
